09675f364c323d3e017fee5e7bdf5e585e0cdb69
yoonhero <yoonhero06@naver.com>
2021-11-02 07:58:35 +0900

    feat: Header #1

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
-  return <h1>Go Go!</h1>;
+  return (
+    <>
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Nav */}
+
+      {/* Result */}
+    </>
+  );
 }
