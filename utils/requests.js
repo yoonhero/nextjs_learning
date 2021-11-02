69f6a63fbf8adf3d076056f00a1fad1db0acd514
yoonhero <yoonhero06@naver.com>
2021-11-02 19:55:13 +0900

    feat: Get Server Side Props

--- utils/requests.js
+++ utils/requests.js
@@ -1,37 +1,14 @@
-const API_KEY = process.env.API_KEY;
+// const API_KEY = process.env.API_KEY;
+const API_KEY = "9c4e72909e805980f8e97aa8af02d8b4";
 
 export default {
   fetchTrending: {
     title: "Trending",
-    url: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
   fetchTopRated: {
     title: "Top Rated",
-    url: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
-  },
-  fetchActionMovies: {
-    title: "Action",
-    url: `/discover/movie?api_key=${API_KEY}?with_genres=28`,
-  },
-  fetchTrending: {
-    title: "Trending",
-    url: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
-  },
-  fetchTopRated: {
-    title: "Top Rated",
-    url: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
-  },
-  fetchActionMovies: {
-    title: "Action",
-    url: `/discover/movie?api_key=${API_KEY}?with_genres=28`,
-  },
-  fetchTrending: {
-    title: "Trending",
-    url: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
-  },
-  fetchTopRated: {
-    title: "Top Rated",
-    url: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   },
   fetchActionMovies: {
     title: "Action",
