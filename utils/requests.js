a65d86f31b157a8da830435922e157ae6496361c
yoonhero <yoonhero06@naver.com>
2021-11-02 17:57:47 +0900

    feat: Nav Bar Finish

--- utils/requests.js
+++ utils/requests.js
@@ -13,4 +13,28 @@ export default {
     title: "Action",
     url: `/discover/movie?api_key=${API_KEY}?with_genres=28`,
   },
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}?with_genres=28`,
+  },
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}$language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}$language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie?api_key=${API_KEY}?with_genres=28`,
+  },
 };
